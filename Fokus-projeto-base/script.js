a560bf86ed03e9ec74a27a2676efa048c7addd03
Patricia Assaf <assafpati@gmail.com>
2024-12-18 17:26:37 -0300

    sons, temporizador

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -4,20 +4,46 @@ const curtoBt = document.querySelector ('.app__card-button--curto')
 const longoBt = document.querySelector ('.app__card-button--longo')
 const banner = document.querySelector ('.app__image')
 const titulo = document.querySelector ('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector ('#alternar-musica')
+const musica = new Audio ('sons/luna-rise-part-one.mp3') 
+const somPlay = new Audio ('sons/play.wav')
+const somPause = new Audio ('sons/pause.mp3')
+const somAlert = new Audio ('sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
+musica.loop = true
+
+musicaFocoInput.addEventListener ('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener ('click', () => {
     alterarContexto ('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener ('click', () => {
     alterarContexto ('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener ('click', () => {
     alterarContexto ('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute ('data-contexto', contexto)
     banner.setAttribute ('src', `imagens/${contexto}.png`)
     switch (contexto) {
@@ -41,3 +67,33 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        somAlert.play() 
+        alert('Tempo finalizado!')
+        zerar()   
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log ('Temporizador:' + tempoDecorridoEmSegundos)
+    console.log ('Id:' + intervaloId)
+}
+
+startPauseBt.addEventListener ('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        somPause.play ()  
+        zerar()              
+        return
+    }
+    somPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar () {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
